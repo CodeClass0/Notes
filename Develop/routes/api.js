d169b31549047ee08ba72276817ace40f0f78199
C A <chadandari@outlook.com>
2024-01-22 17:08:26 -0500

    HTML GET routes functioning

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,7 @@
 const apiRoute = require('express').Router();
 
+//GET NOTES from db.json
+
+//POST NOTES to db.json
 
 module.exports = apiRoute;
--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -1,4 +1,19 @@
 const htmlRoute = require('express').Router();
+const path = require('node:path'); 
 
+//GET NOTES to return notes.html
+//NODE:PATH returns a path string
+htmlRoute.get("/notes", (req, res) =>{
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    console.log("htmlRoute GET notes.html");
+});
+
+
+//GET * return index.html
+//NODE:PATH returns a path string
+htmlRoute.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log("htmlRoute GET index.html");
+});
 
 module.exports = htmlRoute;
