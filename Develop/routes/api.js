65685339594c293beab88e88b828bf901ae62c82
C A <chadandari@outlook.com>
2024-01-22 20:40:31 -0500

    api post works.

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -4,7 +4,7 @@ const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
 const writeToFile = util.promisify(fs.writeFile);
 const { v4: uuidv4 } = require('uuid');
-let old;
+let old = [];
 
 
 //GET NOTES from db.json
@@ -18,6 +18,22 @@ apiRoute.get('/notes', (req, res) => {
 
 //POST NOTES to db.json
 apiRoute.post("/notes", (req,res) => {
+    //Get old data as array of objects
+    readFromFile("db/db.json").then((data) => {
+
+        // old.push(JSON.parse(data));
+        old = JSON.parse(data);
+        let newNote = req.body;
+    
+    //push the new object to the array of old objects.
+       old.push(newNote); 
+
+    //Write new array of objects to db
+    writeToFile("db/db.json", JSON.stringify(old));
+    
+    //respond with the newly updated data
+    res.json(old);
+    });
 
 });
 
