2b0d1034d4026741f66d708c8037d8170ebfca6e
C A <chadandari@outlook.com>
2024-01-22 19:34:17 -0500

    api GET working, api POST not

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,28 @@
 const apiRoute = require('express').Router();
+const fs = require ('fs');
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
+const { v4: uuidv4 } = require('uuid');
+let old;
+
 
 //GET NOTES from db.json
+apiRoute.get('/notes', (req, res) => {
+    readFromFile("db/db.json").then((data) => {
+        console.log("apiRoute GET receiving data from db.json");
+        res.json(JSON.parse(data));
+    });
+});
+
 
 //POST NOTES to db.json
+apiRoute.post("/notes", (req,res) => {
+
+});
+
+
+
+
 
 module.exports = apiRoute;
